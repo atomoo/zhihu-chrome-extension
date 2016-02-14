20a0a3b1c9350f891e199236312a53048627aea6
atomoo <yangpein@gmail.com>
2016-02-14 12:16:29 +0800

    .

--- extension/ex-zhihu.js
+++ extension/ex-zhihu.js
@@ -5,38 +5,38 @@
         if (_url == 'https://www.zhihu.com/') {
             // 首页
             var answersContainers = document.getElementsByClassName('zm-item-rich-text');
-            // if(answersContainers){
-            //     for(var i=0; i<answersContainers.length; i++){
-            //         var contentHidden = answersContainers[i].getElementsByClassName('content')[0].value;
-            //         contentHidden = contentHidden.replace(/<img/g, '<a href="javascrip:void(0)" class="ex-zh-display">显示图片</a><img style="display:none"');
-            //         answersContainers[i].getElementsByClassName('content')[0].value = contentHidden;
-            //     }
-            //     var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
+            if(answersContainers){
+                for(var i=0; i<answersContainers.length; i++){
+                    var contentHidden = answersContainers[i].getElementsByClassName('content')[0].value;
+                    contentHidden = contentHidden.replace(/<img/g, '<a href="javascrip:void(0)" class="ex-zh-display">显示图片</a><img style="display:none"');
+                    answersContainers[i].getElementsByClassName('content')[0].value = contentHidden;
+                }
+                var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
 
-            //     // 选择目标节点
-            //     var targets = document.getElementsByClassName('zm-item-rich-text');
+                // 选择目标节点
+                var targets = document.getElementsByClassName('zm-item-rich-text');
 
-            //     // 创建观察者对象
-            //     var observer = new MutationObserver(function(mutations) {
-            //       mutations.forEach(function(mutation) {
-            //         var btns = mutation.target.getElementsByClassName('ex-zh-display');
-            //         for (var btnIt = btns.length - 1; btnIt >= 0; btnIt--) {
-            //             btns[btnIt].addEventListener('click', function(e){
-            //                 e.preventDefault();
-            //                 e.target.nextSibling.style.display = 'block';
-            //             })
-            //         }
-            //       });    
-            //     });
+                // 创建观察者对象
+                var observer = new MutationObserver(function(mutations) {
+                  mutations.forEach(function(mutation) {
+                    var btns = mutation.target.getElementsByClassName('ex-zh-display');
+                    for (var btnIt = btns.length - 1; btnIt >= 0; btnIt--) {
+                        btns[btnIt].addEventListener('click', function(e){
+                            e.preventDefault();
+                            e.target.nextSibling.style.display = 'block';
+                        })
+                    }
+                  });    
+                });
 
-            //     // 配置观察选项:
-            //     var config = {childList: true}
+                // 配置观察选项:
+                var config = {childList: true}
 
-            //     // 传入目标节点和观察选项
-            //     for (var tarIt = targets.length - 1; tarIt >= 0; tarIt--) {
-            //         observer.observe(targets[tarIt], config);
-            //     };
-            // }
+                // 传入目标节点和观察选项
+                for (var tarIt = targets.length - 1; tarIt >= 0; tarIt--) {
+                    observer.observe(targets[tarIt], config);
+                };
+            }
         } else if (_url.indexOf('question') > -1) {
             // 问题页面
             var richDoms = document.getElementsByClassName('zm-item-rich-text');
